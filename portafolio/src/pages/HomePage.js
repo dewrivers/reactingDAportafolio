9071ae8b48f43fdf68c42a2d7459822b3ae10d09
Rocio Rios <rrocio46@yahoo.es>
2020-11-28 23:08:03 -0500

    making the footer

--- portafolio/src/pages/HomePage.js
+++ portafolio/src/pages/HomePage.js
@@ -1,8 +1,12 @@
 import React from 'react';
+// import Hero from '../components/Hero';
+// import Carousel from '../components/Carousel';
+
 const HomePage = (props) => {
    return(
        <div>
-           
+            {/* <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
+            <Carousel /> */}
        </div>
    )
 }
