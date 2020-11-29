6687e539a09bfbd4e4ec3e9aced13bbdcdc36a80
Rocio Rios <rrocio46@yahoo.es>
2020-11-28 23:45:59 -0500

    routing the pages

--- portafolio/src/pages/HomePage.js
+++ portafolio/src/pages/HomePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-// import Hero from '../components/Hero';
+import Hero from '../components/Hero';
 // import Carousel from '../components/Carousel';
 
 const HomePage = (props) => {
    return(
        <div>
-            {/* <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
-            <Carousel /> */}
+            <Hero title={props.title} subTitle={props.subTitle} text={props.text} />
+            {/* <Carousel /> */}
        </div>
    )
 }
